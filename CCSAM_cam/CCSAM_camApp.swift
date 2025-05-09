082d707285a6f654090288d4c34e64091f84146b
Tikhonov_r <Ruslan.Tikhonov@kaspersky.com>
2025-05-09 11:22:16 +0300

    Initial Commit

---
+++ CCSAM_cam/CCSAM_camApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCSAM_camApp.swift
+//  CCSAM_cam
+//
+//  Created by Руслан Тихонов on 09.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct CCSAM_camApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CCSAM_camTests/CCSAM_camTests.swift
@@ -0,0 +1,17 @@
+//
+//  CCSAM_camTests.swift
+//  CCSAM_camTests
+//
+//  Created by Руслан Тихонов on 09.05.2025.
+//
+
+import Testing
+@testable import CCSAM_cam
+
+struct CCSAM_camTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
